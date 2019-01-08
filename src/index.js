b593ee27a9334e1a8458c70e8299cb259328f810
JZ <justin.qian.v@gmail.com>
2019-01-09 02:06:45 +0800

    First Commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { createStore } from "redux";
+import todoApp from "./reducers";
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+  VisibilityFilters
+} from "./actions";
+const store = createStore(todoApp);
+
+// Log the initial state
+// console.log(store.getState());
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
+// Dispatch some actions
+store.dispatch(addTodo("Learn about actions"));
+store.dispatch(addTodo("Learn about reducers"));
+store.dispatch(addTodo("Learn about store"));
+store.dispatch(toggleTodo(0));
+store.dispatch(toggleTodo(1));
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+store.dispatch(addTodo("Learn react & redux"));
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_ALL));
+// Stop listening to state updates
+unsubscribe();
